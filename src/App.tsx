8179f6a88311bbfc70f3ee38fa3816245dba3fa6
Pretty Dev <53688893+prettydev@users.noreply.github.com>
2020-12-07 07:29:45 -0800

    add server domain

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import socketIOClient from "socket.io-client";
-const ENDPOINT = "http://localhost:4001";
+const ENDPOINT = "http://www.superhry.cz:8500";
 
 function App() {
   const [response, setResponse] = useState<Date>();
@@ -13,9 +13,12 @@ function App() {
   }, []);
 
   return (
-    <p className="bg-red-500">
-      It's {response}
-    </p>
+    <div className="w-1/3 mx-auto my-12 text-center flex flex-col gap-8">
+      <div className="text-5xl font-semibold text-red-500">Hello World</div>
+      <p className="text-lg text-gray-500">
+        It's {response}
+      </p>
+    </div>
   );
 }
 
